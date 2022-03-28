bb15dec093f772ff9a8b8c5902e56c40d0473e5d
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-03-28 21:48:31 +0200

    feat: product component at home

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
+
 import { StyledProduct } from './Product.Styled';
-import img from './images/4.jpg';
 
 
-const Product = ({ page }) => {
-    console.log(page);
+const Product = ({ page, productData }) => {
+    const [quantity, setQuantity] = useState('')
+
+    const addToCart = () => {
+
+    }
+
     return (
         <StyledProduct page={page} >
-            <img src={img} alt="" />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere cum illo dolores ut maxime assumenda delectus repudiandae corporis doloremque enim. Repudiandae, dolores aliquam. Assumenda, eaque.</p>
-            <input type="number" />
-            <button>{page == "home" ? "Ajouter au panier" : "Supprimer le produit"}</button>
-        </StyledProduct>
+            <img src={productData.thumbnailUrl} alt="" />
+            <p >{productData.title}</p>
+            <input type="number" value={quantity} onChange={e => setQuantity(e.target.value)} />
+            <button onClick={() => { addToCart() }}>{page == "home" ? "Ajouter au panier" : "Supprimer le produit"}</button>
+        </StyledProduct >
     )
 }
 
 export default Product;
+
