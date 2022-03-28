d51e77d48b888e8e29583f5b423a9d4050e2034d
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-03-28 21:45:54 +0200

    feat: get products from api

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,29 @@
+import axios from "axios"
+import { useState, useEffect } from "react";
+
 import Product from '../product/Product';
 import { StyledHome, StyledTitle } from './Home.styled';
 
+
 const Home = () => {
+    const [productList, setProductList] = useState("");
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/photos?albumId=1')
+            .then((response) => {
+                setProductList(response.data);
+            })
+    }, []);
+
     return (
         <>
             <StyledTitle>Mes produits</StyledTitle>
             <StyledHome>
-                <Product page="home" />
-                <Product page="home" />
-                <Product page="home" />
-                <Product page="home" />
-
+                {productList && productList.map(product => {
+                    return (
+                        <Product page="home" productData={product} key={product.id} />
+                    )
+                })}
             </StyledHome>
         </>
     )
