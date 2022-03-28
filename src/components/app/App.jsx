844a9ab92d305b8d986825512f29f65034379546
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-03-28 21:50:43 +0200

    feat: setting up CartContext

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,22 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+
+import CartContext from '../../context/CartContext';
 import Container from '../Layout/container/Container';
 import Home from '../home/Home';
-import { Routes, Route } from 'react-router-dom';
 import Cart from '../cart/Cart';
 import { GlobalStyle } from '../../styles/GlobalStyle';
 
 
 function App() {
+
+  const [cartContent, setCartContent] = useState('');
+  const contextValue = {
+    cartContent: cartContent,
+    updateCartContent: setCartContent
+  }
+
   return (
     <>
-      <Container>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-
-        </Routes>
-        {/* <Home /> */}
-      </Container>
+      <CartContext.Provider value={contextValue}>
+        <Container>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Container>
+      </CartContext.Provider>
     </>
   );
 }
